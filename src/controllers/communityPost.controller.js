d0bce7c8728d49634644ce7e901c5c246e718de3
Karan Kumar Gupta <karangupta98335@gmail.com>
2026-06-11 23:24:05 +0530

    added get update and delte controller for commmunity post

--- src/controllers/communityPost.controller.js
+++ src/controllers/communityPost.controller.js
@@ -24,3 +24,154 @@ const createPost = asyncHandler(async (req, res) => {
         .json(new ApiResponse(201, post, "Post created successfully"));
 });
 
+const getAllPosts = asyncHandler(async (req, res) => {
+    const { page = 1, limit = 20, tag, status } = req.query;
+    const query = {};
+
+
+    if (tag) {
+        query.tags = {$regex : tag,$options : "i"};
+
+    }
+
+
+    if (status === "resolved") query.isResolved = true;
+    if (status === "unresolved") query.isResolved = false;
+
+    const posts = await CommunityPost.find(query)
+    .sort({createdAt:-1})
+    .skip((page-1)*limit)
+    .limit(parseInt(limit))
+    .populate("authorId","name avatar");
+    
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, posts, "Community feed fetched successfully"));
+});
+
+
+const getPostById = asyncHandler(async (req, res) => {
+    const { postId } = req.params;
+
+    const post = await CommunityPost.findById(postId).populate("authorId", "name avatar");
+
+    if (!post) {
+        throw new ApiError(404, "Post not found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, post, "Post details fetched successfully"));
+});
+
+
+const updatePost = asyncHandler(async (req, res) => {
+    const { postId } = req.params;
+    const { title, content, tags } = req.body;
+
+    const post = await CommunityPost.findById(postId);
+
+    if (!post) {
+        throw new ApiError(404, "Post not found");
+    }
+
+
+    if (post.authorId.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You can only edit your own posts");
+    }
+
+    if (title) post.title = title.trim();
+    if (content) post.content = content.trim();
+    if (tags) post.tags = tags;
+
+    await post.save();
+    await post.populate("authorId", "name avatar");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, post, "Post updated successfully"));
+});
+
+const deletePost = asyncHandler(async (req, res) => {
+    const { postId } = req.params;
+
+    const post = await CommunityPost.findById(postId);
+
+    if (!post) {
+        throw new ApiError(404, "Post not found");
+    }
+
+    if (post.authorId.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You can only delete your own posts");
+    }
+
+    await CommunityPost.deleteOne({ _id: post._id });
+
+    // i will also  delete all Comments associated with this postId here later after creating it!
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Post deleted successfully"));
+});
+
+// 6. Toggle Upvote (Like / Unlike)
+const toggleUpvote = asyncHandler(async (req, res) => {
+    const { postId } = req.params;
+    const userId = req.user._id;
+
+    const post = await CommunityPost.findById(postId);
+
+    if (!post) {
+        throw new ApiError(404, "Post not found");
+    }
+
+    const hasUpvoted = post.upvotedBy.includes(userId);
+
+    if (hasUpvoted) {
+        // User already upvoted, so we remove their ID (Unlike)
+        post.upvotedBy.pull(userId);
+    } else {
+        // User hasn't upvoted, so we add their ID (Like)
+        post.upvotedBy.push(userId);
+    }
+
+    await post.save();
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { upvotes: post.upvotedBy.length }, hasUpvoted ? "Upvote removed" : "Post upvoted"));
+});
+
+// 7. Toggle Resolved Status (Only author)
+const toggleResolved = asyncHandler(async (req, res) => {
+    const { postId } = req.params;
+
+    const post = await CommunityPost.findById(postId);
+
+    if (!post) {
+        throw new ApiError(404, "Post not found");
+    }
+
+    // Security: Only the author can mark it resolved
+    if (post.authorId.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "Only the author can mark this post as resolved");
+    }
+
+    post.isResolved = !post.isResolved; // Flip the boolean
+    await post.save();
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { isResolved: post.isResolved }, post.isResolved ? "Marked as resolved" : "Marked as unresolved"));
+});
+
+export { 
+    createPost, 
+    getAllPosts, 
+    getPostById, 
+    updatePost, 
+    deletePost, 
+    toggleUpvote, 
+    toggleResolved 
+};
--- src/routes/communityPost.router.js
+++ src/routes/communityPost.router.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { createPost } from "../controllers/communityPost.controller.js";
+import { createPost, deletePost, getAllPosts, getPostById, updatePost } from "../controllers/communityPost.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/create-post").post(verifyJWT,createPost);
+router.route("/get-all-post").get(verifyJWT,getAllPosts);
+router.route("/get-post/:postId").get(verifyJWT,getPostById);
+router.route("/update-post/:postId").patch(verifyJWT,updatePost);
+router.route("/delete-post/:postId").delete(verifyJWT,deletePost);
 
 export default router;
